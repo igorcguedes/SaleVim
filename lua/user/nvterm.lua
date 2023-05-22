0b6fb308b143a71c81bf2d1745673a4356e16c8e
igorcguedes <igor.g1999@hotmail.com>
2023-05-22 19:40:13 -0300

    first commit

---
+++ lua/user/nvterm.lua
@@ -0,0 +1,15 @@
+require("nvterm").setup()
+
+local terminal = require("nvterm.terminal")
+
+local toggle_modes = {'n', 't'}
+local mappings = {
+  { 'n', '<C-l>', function () require("nvterm.terminal").send(ft_cmds[vim.bo.filetype]) end },
+  { toggle_modes, '<A-h>', function () require("nvterm.terminal").toggle('horizontal') end },
+  { toggle_modes, '<A-v>', function () require("nvterm.terminal").toggle('vertical') end },
+  { toggle_modes, '<A-i>', function () require("nvterm.terminal").toggle('float') end },
+}
+local opts = { noremap = true, silent = true }
+for _, mapping in ipairs(mappings) do
+  vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)
+end
---
+++ lua/user/webdev.lua
@@ -0,0 +1,28 @@
+--require("nvim-web-devicons").set_default_icon('󰢎"', '#6d8086', 65)
+require("nvim-web-devicons").set_icon {
+  cls = {
+    icon = "󰢎 ",
+    color = "#1798c1",
+    name = "cls"
+  },
+  cmp = {
+    icon = " ",
+    color = "#e34c26",
+    name = "cmp"
+  },
+  page = {
+    icon = " ",
+    color = "#e34c26",
+    name = "page"
+  },
+  design = {
+    icon = " ",
+    color = "#e34c26",
+    name = "design"
+  },
+  trigger = {
+    icon = "󰢎 ",
+    color = "#1798c1",
+    name = "trigger"
+  }
+}
